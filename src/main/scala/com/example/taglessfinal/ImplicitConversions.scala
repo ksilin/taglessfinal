9926c46fc85c622e13e34a3794c56932072573ca
Konstantin Silin <ksilin@multivac.fritz.box>
2017-08-12 13:59:56 +0200

    generic monad transform with natural transformation

--- src/main/scala/com/example/taglessfinal/ImplicitConversions.scala
+++ src/main/scala/com/example/taglessfinal/ImplicitConversions.scala
@@ -1,6 +1,6 @@
 package com.example.taglessfinal
 
-import cats.Id
+import cats.{ Id, Monad }
 
 import scala.concurrent.Future
 
@@ -8,6 +8,16 @@ object ImplicitConversions {
 
   implicit class IdToFuture[A](id: Id[A]) {
     def toFuture: Future[A] = Future.successful(id)
+  }
+
+  import cats.~>
+  implicit class NaturalTransformation[F[_], A](fa: F[A]) {
+    def liftTo[G[_]](implicit transform: F ~> G): G[A] =
+      transform(fa)
+  }
+
+  implicit def idToMonad[M[_]: Monad] = new (Id ~> M) {
+    override def apply[A](a: Id[A]): M[A] = Monad[M].pure(a)
   }
 
 }
--- src/main/scala/com/example/taglessfinal/RepoFutureIdInterpreter.scala
+++ src/main/scala/com/example/taglessfinal/RepoFutureIdInterpreter.scala
@@ -1,28 +1,30 @@
 package com.example.taglessfinal
 
 import cats.Id
+import cats.implicits._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import com.example.taglessfinal.ImplicitConversions.IdToFuture
+import com.example.taglessfinal.ImplicitConversions.NaturalTransformation
+import com.example.taglessfinal.ImplicitConversions.idToMonad
 
 class RepoFutureIdInterpreter(logger: Logger[Id]) extends ProductRepo[Future] {
 
   override def findProduct(id: ProductId): Future[Option[Product]] =
     for {
-      _    <- logger.info(s"looking for product $id").toFuture
+      _    <- logger.info(s"looking for product $id").liftTo[Future] //toFuture
       prod <- Future.successful(Some(Product(id, "default")))
     } yield prod
 
   override def storeProduct(p: Product): Future[Unit] =
     for {
-      _ <- logger.info(s"storing product $p").toFuture
+      _ <- logger.info(s"storing product $p").liftTo[Future] //#(idToMonad) //toFuture
 //      prod <- Future.successful(())
     } yield ()
 
   override def incrementProductSales(productId: ProductId, amount: Int): Future[Unit] =
     for {
-      _ <- logger.info(s"incrementing sales for $productId").toFuture
+      _ <- logger.info(s"incrementing sales for $productId").liftTo[Future] //toFuture
 //      prod <- Future.successful(())
     } yield ()
 }
